13c5e7180f1ab83e3591203e97419883cac0175a
Zachary Robin <zacharyrobin@Zacharys-MacBook-Pro-2.local>
2019-04-01 11:56:08 -0400

    asdf

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/Articles-db", { useNewUrlParser: true });
 
@@ -46,6 +47,8 @@ app.get("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
+      
+       
         //console.log(result.title);
         //console.log(result.link);
       // Create a new Article using the `result` object built from scraping
